04e30cdb5ffb6e16397ab18f6e2f74ea711885df
saharBaribi <57773733+saharBaribi@users.noreply.github.com>
2021-04-20 17:46:30 +0300

    adding validation, regularization, stopping criteria and more

--- DeepLearning/ex2/ex2.py
+++ DeepLearning/ex2/ex2.py
@@ -19,34 +19,49 @@ import sys
 from keras.optimizers import Adam
 from tensorflow.keras.regularizers import l2, l1
 from keras import backend as K
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.initializers import RandomNormal
 
 FILES_PATH = 'lfw2/lfw2/'
 TRAIN_PATH = 'Train.txt'
 TEST_PATH = 'Test.txt'
 
-IMAGE_SIZE = (250, 250)
+IMAGE_SIZE = (105, 105)
 TRAIN_SIZE = 2200
-BATCH_SIZE = 32
+BATCH_SIZE = 64
+VAL_SPLIT = 0.2
 
+STEP_SIZE_TRAIN=(TRAIN_SIZE*(1-VAL_SPLIT))//BATCH_SIZE
+STEP_SIZE_VALID= int(TRAIN_SIZE*VAL_SPLIT)//BATCH_SIZE
+EPOCHS_TO_DECAY = 2
 
-def create_gen(data, datagen, train=True):
+weight_initialize = RandomNormal(mean=0.0, stddev=0.01, seed=None)
+dense_weight_initialize = RandomNormal(mean=0.0, stddev=0.02, seed=None)
+bias_initialize = RandomNormal(mean=0.5, stddev=0.01, seed=None)
+
+def create_gen(data, datagen, train=True, val=False):
     if train:
         class_mode = 'binary'
         y_col = 'label'
         batch_size = BATCH_SIZE
+        subset = 'training'
+        if val:
+            subset = 'validation'
     else:
         batch_size = 1
         class_mode = 'binary'
         y_col = 'label'
+        subset = None
 
     gen1 = datagen.flow_from_dataframe(dataframe=data, target_size=IMAGE_SIZE, x_col='image1',
                                        y_col=y_col, color_mode="rgb",
-                                       class_mode=class_mode, subset='training', shuffle=False,
+                                       class_mode=class_mode, subset=subset, shuffle=False,
                                        batch_size=batch_size, seed=42)
 
     gen2 = datagen.flow_from_dataframe(dataframe=data, target_size=IMAGE_SIZE, x_col='image2',
                                        y_col=y_col, color_mode="rgb",
-                                       class_mode=class_mode, subset='training', shuffle=False,
+                                       class_mode=class_mode, subset=subset, shuffle=False,
                                        batch_size=batch_size, seed=42)
 
     while True:
@@ -55,7 +70,6 @@ def create_gen(data, datagen, train=True):
 
         yield [image_1[0], image_2[0]], image_1[1]
 
-
 def create_path(tup):
     name, num = tup
     padding = 4
@@ -72,7 +86,6 @@ def create_path(tup):
     file_name = name + '_' + num + '.jpg'
     path = os.path.join(FILES_PATH, name, file_name)
     return path
-
 
 def read_data(path, n=1100):
     count = 0
@@ -94,22 +107,21 @@ def read_data(path, n=1100):
 def calculate_distance(tensors):
     return abs(tensors[0] - tensors[1])
 
-
 ## Creating the model:
 def create_model():
     input1 = Input(IMAGE_SIZE +(3,))
     input2 = Input(IMAGE_SIZE+(3,))
 
     model = Sequential()
-    model.add(Conv2D(64, (10, 10), activation='relu', input_shape=IMAGE_SIZE+(3,)))
+    model.add(Conv2D(64, (10, 10), activation='relu', input_shape=IMAGE_SIZE+(3,), kernel_regularizer=l2(2e-4),kernel_initializer = weight_initialize))
     model.add(MaxPooling2D())
-    model.add(Conv2D(128, (7, 7), activation='relu', kernel_regularizer=l2(2e-4)))
+    model.add(Conv2D(128, (7, 7), activation='relu', kernel_regularizer=l2(2e-4), kernel_initializer = weight_initialize, bias_initializer=bias_initialize))
     model.add(MaxPooling2D())
-    model.add(Conv2D(128, (4, 4), activation='relu', kernel_regularizer=l2(2e-4)))
+    model.add(Conv2D(128, (4, 4), activation='relu', kernel_regularizer=l2(2e-4), kernel_initializer = weight_initialize, bias_initializer=bias_initialize))
     model.add(MaxPooling2D())
-    model.add(Conv2D(256, (4, 4), activation='relu', kernel_regularizer=l2(2e-4)))
+    model.add(Conv2D(256, (4, 4), activation='relu', kernel_regularizer=l2(2e-4), kernel_initializer = weight_initialize, bias_initializer=bias_initialize))
     model.add(Flatten())
-    model.add(Dense(4096, activation='sigmoid'))
+    model.add(Dense(4096, activation='sigmoid', kernel_regularizer=l2(2e-4), kernel_initializer = dense_weight_initialize, bias_initializer=bias_initialize))
 
     encoded1 = model(input1)
     encoded2 = model(input2)
@@ -155,8 +167,7 @@ def n_way_one_shot(n, data, model, datagen):
 
 
 def test_n_way(model, n_way_gen, n):
-    # TODO: check if should be predict proba
-    probabilities = model.predict(n_way_gen, steps=n)
+    probabilities = model.predict(n_way_gen, steps = n)
     # In our generator the correct pair is in the first row every time,
     # so we would expect it to receive the max probability
     if np.argmax(probabilities) == 0:
@@ -166,7 +177,7 @@ def test_n_way(model, n_way_gen, n):
 
 
 def select_pairs_to_compare(n, images, name_to_compare):
-    sample_false = images[images['name'] != name_to_compare]['image'].sample(n=n - 1)
+    sample_false = images[images['name'] != name_to_compare]['image'].sample(n=n-1)
     return sample_false
 
 # def read_images():
@@ -190,33 +201,45 @@ def select_pairs_to_compare(n, images, name_to_compare):
 
 # Data Exploration
 # read_images()
-datagen = generator(rescale=1. / 255)
+datagen = generator(rescale=1. / 255, validation_split = VAL_SPLIT)
+# datagen = generator(rescale=1. / 255)
+test_datagen = generator(rescale=1. / 255)
 train_data = read_data(TRAIN_PATH)
 test_data = read_data(TEST_PATH, 500)
 
+
 train_gen = create_gen(train_data, datagen)
-test_gen = create_gen(test_data, datagen)
+val_gen = create_gen(train_data, datagen, val = True)
+test_gen = create_gen(test_data, test_datagen, False)
 
 siamese_network = create_model()
 siamese_network.summary()
-siamese_network.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=[tf.keras.metrics.Accuracy()])
 
+stopping_criteria = EarlyStopping(monitor='val_loss', mode='auto', patience = 3, restore_best_weights=True)
+initial_learning_rate = 0.0001
+lr_schedule = ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=STEP_SIZE_TRAIN*EPOCHS_TO_DECAY,
+    decay_rate=0.99,
+    staircase=True)
+siamese_network.compile(optimizer=Adam(learning_rate=lr_schedule), loss='binary_crossentropy', metrics=['binary_accuracy'])
 # Training the model:
-STEP_SIZE_TRAIN=TRAIN_SIZE//BATCH_SIZE
-
 siamese_network.fit(train_gen,
                     steps_per_epoch=STEP_SIZE_TRAIN,
-                    epochs=30,shuffle=False)
+                    epochs=30,shuffle=False,
+                    validation_data=val_gen,
+                    validation_steps=STEP_SIZE_VALID,
+                    callbacks=[stopping_criteria])
 
 
 # siamese_network.save_weights('.my_checkpoint')
+siamese_network.evaluate(test_gen, steps = len(test_data))
 
+acc = n_way_one_shot(3, train_data, siamese_network, test_datagen)
 
-acc = n_way_one_shot(3, train_data, siamese_network, datagen)
-
-test_acc = n_way_one_shot(3, test_data, siamese_network, datagen)
+test_acc = n_way_one_shot(3, test_data, siamese_network, test_datagen)
 
 print(f"train accuracy: {acc}")
-print(f"test accuracy: {acc}")
+print(f"test accuracy: {test_acc}")
 
 #
