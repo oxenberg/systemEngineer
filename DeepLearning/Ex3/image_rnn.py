fbf9bb2845bd1a8fec428fe3b4b9712ad6d9f8fd
Or Oxenberg <oxenberg14@gmail.com>
2021-05-01 12:55:07 +0300

    fix utils params

--- DeepLearning/Ex3/image_rnn.py
+++ DeepLearning/Ex3/image_rnn.py
@@ -13,25 +13,30 @@ CREATE_IMAGES = True
 
 def plot_piano_roll(pm, name=None):
     ax = plt.axes()
-
+    image_path = f"{IMAGES_PATH}{name}.png"
     # Use librosa's specshow function for displaying the piano roll
     fig = librosa.display.specshow(pm.get_piano_roll(), ax=ax,
                                    hop_length=1, x_axis='time', y_axis='cqt_note')
     ax.axis('off')
-    fig.savefig(fname=f"{IMAGES_PATH}{name}.png", bbox_inches='tight', pad_inches=0)
+    ax.figure.savefig(fname=image_path, bbox_inches='tight', pad_inches=0)
+    return
 
-
-def create_images(data):
-    print(f"---- create {data} in images file ----")
+def create_images(data, name):
+    print(f"---- create {name} in images file ----")
+    images_path = []
     if CREATE_IMAGES:
         for file in tqdm(data["file_name"]):
             try:
                 name = file.split(".")[0]
                 pm = pretty_midi.PrettyMIDI(params["MIDI_FILES_PATH"] + file)
-                plot_piano_roll(pm, name)
+                image_path = plot_piano_roll(pm, name)
+                images_path.append(image_path)
             except KeySignatureError:
                 print(f"Exception: could not create {file}")
-
+                images_path.append(None)
+        data["image_path"] = images_path
+        print(f"save {name} file")
+        data.to_csv(f"{name}.csv")
 
 # main
 
@@ -40,5 +45,5 @@ train = read_lyrics_data(params["TRAIN_FILE"])
 test = read_lyrics_data(params["TEST_FILE"])
 
 # export piano notes spectrogram to png images in images file
-create_images(train)
-create_images(test)
+create_images(train, name="train")
+create_images(test, name="test")
