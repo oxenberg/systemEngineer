a7de0302b6c41f9cdfde8f2da3e5cce2496b53db
saharBaribi <57773733+saharBaribi@users.noreply.github.com>
2021-05-01 18:31:22 +0300

    adding train to the model

--- DeepLearning/Ex3/utils.py
+++ DeepLearning/Ex3/utils.py
@@ -1,22 +1,37 @@
-
 import gensim.downloader
-from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Embedding, LSTM, Input
 
+from tensorflow import convert_to_tensor, concat
+from pretty_midi import PrettyMIDI
+from tensorflow.keras.optimizers import Adam
+from mido.midifiles.meta import KeySignatureError
 
 params = {
     "TEST_FILE": "lyrics_test_set.csv",
     "TRAIN_FILE": "lyrics_train_set.csv",
-    "MIDI_FILES_PATH": "midi_files/"
+    "MIDI_FILES_PATH": "midi_files/",
+    "BATCH_SIZE": 256
 }
+
+
+def extract_midi_vector(midi_file_name):
+    try:
+        m_file = PrettyMIDI(midi_file_name)
+        return convert_to_tensor(m_file.get_pitch_class_histogram(), dtype = np.float32)
+    except (KeySignatureError, OSError, EOFError, ValueError):
+        return None
+
 
 
 def read_lyrics_data(path):
     def concat_name(file_names):
         artist = "_".join(file_names[0].split())
         title = "_".join(file_names[1].split())
-        file_name = f"{artist}_-_{title}.mid"
+        file_name = f"{params['MIDI_FILES_PATH']}{artist}_-_{title}.mid"
         return file_name
 
     data = pd.read_csv(path, sep='\n', header=None)
@@ -28,7 +43,7 @@ def read_lyrics_data(path):
 
 
 def upload_w2v():
-    model = gensim.downloader.load('glove-wiki-gigaword-50')
+    model = gensim.downloader.load('glove-wiki-gigaword-300')
     model.save("word2vec.wordvectors")
     return model
 
@@ -39,5 +54,18 @@ def load_model(path="word2vec.wordvectors"):
 
 
 def get_word_embed(word, model):
-    return model.wv[word]
+    try:
+        return model.wv[word]
+    except KeyError:
+        return None
 
+
+def create_rnn(input_dim, output_dim):
+    model = Sequential()
+    model.add(Input(input_dim))
+    model.add(Dropout(0.1))
+    model.add(LSTM(256))
+    model.add(Dense(output_dim, activation = "softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001))
+
+    return model
--- DeepLearning/Ex3/vectors_rnn.py
+++ DeepLearning/Ex3/vectors_rnn.py
@@ -1,8 +1,64 @@
 from utils import *
+from glob import glob
 
-print("uploading model")
+# print("uploading model")
 # model = upload_w2v()
-model = load_model()
-print("getting vector")
-v = get_word_embed("all", model)
-print(v)
+
+# print("getting vector")
+# v = get_word_embed("all", model)
+# print(v.shape)
+
+
+
+
+def create_vocab(data):
+    words = set()
+    for i in data["lyrics"]:
+        lyrics = i.lower().split(" ")
+        words.update(lyrics)
+    words.add('<END>')
+    word2index = {w: i for i, w in enumerate(list(words))}
+
+    return word2index
+
+def get_embeddings(word, midi_data, embedding_model):
+    word_vec = get_word_embed(word, embedding_model)
+    if word_vec is None:
+        return None
+    word_vec = convert_to_tensor(word_vec, dtype = np.float32)
+    input = concat([word_vec, midi_data], 0)
+    return input
+
+
+def train_model(train_data, midi_func, embedding_model, input_dim, output_dim, vocab):
+    train_df = pd.DataFrame(columns=["current", "next"])
+    for i, row in train_data.iterrows():
+        midi_data_for_model = midi_func(row['file_name'])
+        if midi_data_for_model is None:
+            continue
+        song_lyrics = row["lyrics"].split(" ")
+        for j in range(len(song_lyrics)-1):
+            word = song_lyrics[j]
+            next_word_index = vocab[song_lyrics[j+1]]
+            input_vec = get_embeddings(word, midi_data_for_model, embedding_model)
+            if input_vec is None:
+                continue
+            train_df.append([[input_vec, next_word_index]])
+        input_vec = get_embeddings(song_lyrics[-1], midi_data_for_model, embedding_model)
+        train_df.append([[input_vec, vocab['<END>']]])
+
+    X = train_df[:, :-1]
+    y = train_df[:, -1]
+    # y = tensorflow.keras.utils.to_categorical(train_df[:, -1], num_classes=len(vocab))
+    lstm = create_rnn(input_dim, output_dim)
+
+    lstm.fit(X, y, batch_size = params["BATCH_SIZE"], epochs = 1, validation_split = 0.25)
+
+
+data = read_lyrics_data(params["TRAIN_FILE"])
+vocab = create_vocab(data)
+vocab_size = len(vocab)
+input_dim = (312, )
+embedding_model = load_model()
+
+train_model(data, extract_midi_vector, embedding_model, input_dim, vocab_size, vocab)
