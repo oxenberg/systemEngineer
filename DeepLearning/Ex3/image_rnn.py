637d82eb149edc165979d9aba54881af28275223
Or Oxenberg <oxenberg14@gmail.com>
2021-05-01 12:34:42 +0300

    fix utils params

--- DeepLearning/Ex3/image_rnn.py
+++ DeepLearning/Ex3/image_rnn.py
@@ -3,25 +3,42 @@ import glob
 import pretty_midi
 import matplotlib.pyplot as plt
 import librosa.display
-from .utils import read_lyrics_data
-TEST_FILE = "lyrics_test_set.csv"
-TRAIN_FILE = "lyrics_train_set.csv"
-MIDI_FILES_PATH = "midi_files/"
+from utils import read_lyrics_data, params
+from tqdm import tqdm
+from mido.midifiles.meta import KeySignatureError
 
-def plot_piano_roll(pm):
+IMAGES_PATH = "images/"
+CREATE_IMAGES = True
+
+
+def plot_piano_roll(pm, name=None):
+    ax = plt.axes()
+
     # Use librosa's specshow function for displaying the piano roll
-    librosa.display.specshow(pm.get_piano_roll(),
-                             hop_length=1, x_axis='time', y_axis='cqt_note')
-    plt.show()
+    fig = librosa.display.specshow(pm.get_piano_roll(), ax=ax,
+                                   hop_length=1, x_axis='time', y_axis='cqt_note')
+    ax.axis('off')
+    fig.savefig(fname=f"{IMAGES_PATH}{name}.png", bbox_inches='tight', pad_inches=0)
 
 
+def create_images(data):
+    print(f"---- create {data} in images file ----")
+    if CREATE_IMAGES:
+        for file in tqdm(data["file_name"]):
+            try:
+                name = file.split(".")[0]
+                pm = pretty_midi.PrettyMIDI(params["MIDI_FILES_PATH"] + file)
+                plot_piano_roll(pm, name)
+            except KeySignatureError:
+                print(f"Exception: could not create {file}")
 
 
+# main
 
+# read data from the test train files (not the mid files)
+train = read_lyrics_data(params["TRAIN_FILE"])
+test = read_lyrics_data(params["TEST_FILE"])
 
-train = read_lyrics_data(TRAIN_FILE)
-test = read_lyrics_data(TEST_FILE)
-
-for file in train["file_name"]:
-    pm = pretty_midi.PrettyMIDI(MIDI_FILES_PATH+file)
-    plot_piano_roll(pm)
+# export piano notes spectrogram to png images in images file
+create_images(train)
+create_images(test)
